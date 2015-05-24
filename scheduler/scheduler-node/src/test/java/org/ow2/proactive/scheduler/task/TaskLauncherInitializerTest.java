6d80242fa1b6309df23f83de3045fde298e57f4c
youribonnaffe <youri.bonnaffe@activeeon.com>
2015-05-24 18:54:53 +0200

    Add unit tests for dataspace selector filtering

--- scheduler/scheduler-node/src/test/java/org/ow2/proactive/scheduler/task/TaskLauncherInitializerTest.java
+++ scheduler/scheduler-node/src/test/java/org/ow2/proactive/scheduler/task/TaskLauncherInitializerTest.java
@@ -1,29 +1,99 @@
 package org.ow2.proactive.scheduler.task;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.ow2.proactive.scheduler.common.task.dataspaces.FileSelector;
-import org.ow2.proactive.scheduler.common.task.dataspaces.InputAccessMode;
-import org.ow2.proactive.scheduler.common.task.dataspaces.InputSelector;
+import org.ow2.proactive.scheduler.common.task.dataspaces.*;
 
 import java.io.Serializable;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 public class TaskLauncherInitializerTest {
 
-    // TODO write more tests
+    private TaskLauncherInitializer initializer;
+
+    @Before
+    public void setUp() {
+        initializer = new TaskLauncherInitializer();
+    }
+
     @Test
-    public void testName() throws Exception {
+    public void input_files_can_be_filtered() throws Exception {
+        initializer.setTaskInputFiles(inputSelectors("$TEST/a", "b"));
+        initializer.getTaskInputFiles().get(0).getInputFiles().setExcludes("$TEST/excluded");
 
-        TaskLauncherInitializer initializer = new TaskLauncherInitializer();
-        initializer.setTaskInputFiles(Collections.singletonList(new InputSelector(new FileSelector("$TEST/a", "b"),
-                InputAccessMode.TransferFromInputSpace)));
+        List<InputSelector> filteredInputFiles = initializer.getFilteredInputFiles(
+                Collections.<String, Serializable>singletonMap("TEST", "folder"));
 
-        List<InputSelector> filteredInputFiles = initializer.getFilteredInputFiles(Collections.<String, Serializable>singletonMap("TEST", "folder"));
+        InputSelector selector = filteredInputFiles.get(0);
+        assertArrayEquals(new String[]{"folder/a", "b"}, selector.getInputFiles().getIncludes());
+        assertArrayEquals(new String[]{"folder/excluded"}, selector.getInputFiles().getExcludes());
+    }
 
-        InputSelector inputSelector = filteredInputFiles.get(0);
-        assertEquals("folder/a", inputSelector.getInputFiles().getIncludes()[0]);
+    @Test
+    public void output_files_can_be_filtered() throws Exception {
+        initializer.setTaskOutputFiles(outputSelectors("${TEST}/a", "b"));
+        initializer.getTaskOutputFiles().get(0).getOutputFiles().setExcludes("$TEST/excluded");
+
+        List<OutputSelector> filteredOutputFiles = initializer.getFilteredOutputFiles(
+                Collections.<String, Serializable>singletonMap("TEST", "folder"));
+
+        OutputSelector selector = filteredOutputFiles.get(0);
+        assertArrayEquals(new String[]{"folder/a", "b"}, selector.getOutputFiles().getIncludes());
+        assertArrayEquals(new String[]{"folder/excluded"}, selector.getOutputFiles().getExcludes());
+    }
+
+    @Test
+    public void output_empty_filters() throws Exception {
+        initializer.setTaskOutputFiles(outputSelectors("$TEST/a", "b"));
+
+        List<OutputSelector> filteredOutputFiles = initializer.getFilteredOutputFiles(
+                Collections.<String, Serializable>emptyMap());
+
+        OutputSelector selector = filteredOutputFiles.get(0);
+        assertArrayEquals(new String[]{"$TEST/a", "b"}, selector.getOutputFiles().getIncludes());
+    }
+
+    @Test
+    public void input_empty_filters() throws Exception {
+        initializer.setTaskInputFiles(inputSelectors("$TEST/a", "b"));
+
+        List<InputSelector> filteredInputFiles = initializer.getFilteredInputFiles(
+                Collections.<String, Serializable>emptyMap());
+
+        InputSelector selector = filteredInputFiles.get(0);
+        assertArrayEquals(new String[]{"$TEST/a", "b"}, selector.getInputFiles().getIncludes());
+    }
+
+    @Test
+    public void input_null_filters() throws Exception {
+        initializer.setTaskInputFiles(inputSelectors("$TEST/a", "b"));
+
+        List<InputSelector> filteredInputFiles = initializer.getFilteredInputFiles(null);
+
+        InputSelector selector = filteredInputFiles.get(0);
+        assertArrayEquals(new String[]{"$TEST/a", "b"}, selector.getInputFiles().getIncludes());
+    }
+
+    @Test
+    public void output_null_filters() throws Exception {
+        initializer.setTaskOutputFiles(outputSelectors("$TEST/a", "b"));
+
+        List<OutputSelector> filteredOutputFiles = initializer.getFilteredOutputFiles(null);
+
+        OutputSelector selector = filteredOutputFiles.get(0);
+        assertArrayEquals(new String[]{"$TEST/a", "b"}, selector.getOutputFiles().getIncludes());
+    }
+
+    private List<InputSelector> inputSelectors(String... selectors) {
+        return Collections.singletonList(new InputSelector(new FileSelector(selectors),
+                InputAccessMode.TransferFromUserSpace));
+    }
+
+    private List<OutputSelector> outputSelectors(String... selectors) {
+        return Collections.singletonList(new OutputSelector(new FileSelector(selectors),
+                OutputAccessMode.TransferToUserSpace));
     }
 }
